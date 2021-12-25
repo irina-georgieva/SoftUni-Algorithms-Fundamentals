b692ea94b8a133c21c41b3d2c5af83660ac3bece
Irina Georgieva <georgieva2490@gmail.com>
2021-12-26 00:22:00 +0100

    Added new solutions

---
+++ 04 Recursion and Combinatorial Algorithms Exercise/RecursionCombinatorialExer/Cinema/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cinema
+{
+    class Program
+    {
+        private static List<string> nonStaticPeople;
+        private static string[] people;
+        private static bool[] locked;
+
+        static void Main(string[] args)
+        {
+            nonStaticPeople = Console.ReadLine().Split(", ").ToList();
+            people = new string[nonStaticPeople.Count];
+            locked = new bool[nonStaticPeople.Count];
+
+            while (true)
+            {
+                var line = Console.ReadLine();
+
+                if (line == "generate")
+                {
+                    break;
+                }
+
+                var parts = line.Split(" - ");
+                var name = parts[0];
+                var position = int.Parse(parts[1]) - 1;
+
+                people[position] = name;
+                locked[position] = true;
+
+                nonStaticPeople.Remove(name);
+            }
+
+            Permute(0);
+        }
+
+        private static void Permute(int index)
+        {
+            if (index >= nonStaticPeople.Count)
+            {
+                PrintPermutation();               
+                return;
+            }
+
+            Permute(index + 1);
+
+            for (int i = index + 1; i < nonStaticPeople.Count; i++)
+            {
+                Swap(index, i);
+                Permute(index + 1);
+                Swap(index, i);
+            }
+        }
+
+        private static void PrintPermutation()
+        {
+            var peopleIndex = 0;
+            for (int i = 0; i < people.Length; i++)
+            {
+                if (i == people.Length - 1)
+                {
+                    if (locked[i])
+                    {
+                        Console.Write($"{people[i]}");
+                    }
+                    else
+                    {
+                        Console.Write($"{nonStaticPeople[peopleIndex++]}");
+                    }
+                }
+                else
+                {
+                    if (locked[i])
+                    {
+                        Console.Write($"{people[i]} ");
+                    }
+                    else
+                    {
+                        Console.Write($"{nonStaticPeople[peopleIndex++]} ");
+                    }
+                }
+            }
+
+            Console.WriteLine();
+        }
+
+        private static void Swap(int first, int second)
+        {
+            var temp = nonStaticPeople[first];
+            nonStaticPeople[first] = nonStaticPeople[second];
+            nonStaticPeople[second] = temp;
+        }
+    }
+}
---
+++ 04 Recursion and Combinatorial Algorithms Exercise/RecursionCombinatorialExer/Reverse Array/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Reverse_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var elements = Console.ReadLine().Split();
+
+            Reverse(elements, 0);
+
+            Console.WriteLine(string.Join(" ", elements));
+        }
+
+        private static void Reverse(string[] elements, int index)
+        {
+            if (index == elements.Length / 2)
+            {
+                return;
+            }
+
+            var temp = elements[index];
+            elements[index] = elements[elements.Length - index - 1];
+
+            elements[elements.Length - index - 1] = temp;
+
+            Reverse(elements, index + 1);
+        }
+    }
+}
