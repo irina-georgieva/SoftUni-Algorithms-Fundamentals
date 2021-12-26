7c12def371c186342435fa2c11523eb492ff09e3
Irina Georgieva <georgieva2490@gmail.com>
2021-12-26 16:17:49 +0100

    Added new solution

--- 04 Recursion and Combinatorial Algorithms Exercise/RecursionCombinatorialExer/School Teams/Program.cs
+++ 04 Recursion and Combinatorial Algorithms Exercise/RecursionCombinatorialExer/School Teams/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace School_Teams
 {
@@ -6,7 +8,45 @@ namespace School_Teams
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var girls = Console.ReadLine().Split(", ");
+            var girlsComb = new string[3];
+            var girlsCombs = new List<string[]>();
+
+            GenCombs(0, 0, girls, girlsComb, girlsCombs);
+
+            var boys = Console.ReadLine().Split(", ");
+            var boysComb = new string[2];
+            var boysCombs = new List<string[]>();
+
+            GenCombs(0, 0, boys, boysComb, boysCombs);
+
+            PrintFinalCombs(girlsCombs, boysCombs);
+        }
+
+        private static void PrintFinalCombs(List<string[]> girlsCombs, List<string[]> boysCombs)
+        {
+            foreach (var girlComb in girlsCombs)
+            {
+                foreach (var boyComb in boysCombs)
+                {
+                    Console.WriteLine($"{string.Join(", ", girlComb)}, {string.Join(", ", boyComb)}");
+                }
+            }
+        }
+
+        private static void GenCombs(int index, int start, string[] elements, string[] comb, List<string[]> combs)
+        {
+            if (index >= comb.Length)
+            {
+                combs.Add(comb.ToArray());
+                return;
+            }
+
+            for (int i = start; i < elements.Length; i++)
+            {
+                comb[index] = elements[i];
+                GenCombs(index + 1, i + 1, elements, comb, combs);
+            }
         }
     }
 }
