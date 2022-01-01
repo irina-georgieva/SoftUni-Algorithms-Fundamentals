694f4a4f3bde26206d3f285a3780701ab800372c
Irina Georgieva <georgieva2490@gmail.com>
2022-01-01 19:42:08 +0100

    Added new solutions

---
+++ 06 SearchingSortingAndGreedyAlgorithms/Lab/Binary Search/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace Binary_Search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(BinarySearch(numbers, number));
+        }
+
+        private static int BinarySearch(int[] numbers, int number)
+        {
+            var left = 0;
+            var right = numbers.Length - 1;
+
+            while (left <= right)
+            {
+                var mid = (left + right) / 2;
+
+                if (numbers[mid] == number)
+                {
+                    return mid;
+                }
+
+                if (number > numbers[mid])
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ 06 SearchingSortingAndGreedyAlgorithms/Lab/Insertion Sort/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace Selection_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            BubbleSort(numbers);
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        private static void BubbleSort(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                var j = i;
+
+                while (j > 0 && numbers[j-1] > numbers[j])
+                {
+                    Swap(numbers, j, j - 1);
+                    j -= 1;
+                }
+            }
+        }
+
+        private static void Swap(int[] numbers, int first, int second)
+        {
+            var temp = numbers[first];
+            numbers[first] = numbers[second];
+            numbers[second] = temp;
+        }
+    }
+}
